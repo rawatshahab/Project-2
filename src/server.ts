ab632d94c15f2f639e4915ec99ff8e0fecfc3383
Mayank Rawat <131944775+rawatshahab@users.noreply.github.com>
2025-04-16 19:35:44 +0530

    changed port

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5006;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
